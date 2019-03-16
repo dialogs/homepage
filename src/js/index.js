d3736a48eb9793ab0d0a3e7a6b9cdea27a8e2816
Ivan Krasnikov <6051852@gmail.com>
2019-03-16 20:26:14 +0300

    feat(home): styling business section finished

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ import $ from 'jquery';
 import './forms.js';
 
 $(function() {
+	console.log('ww onload', $(window).width());
+
 	$(window).on('resize', function(e) {
     console.log($(this), $(this).width());
 	});
