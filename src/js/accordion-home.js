83970cbfc7d099fd5f1f0a04d6150fd2fad25545
Ivan Krasnikov <6051852@gmail.com>
2019-03-18 13:31:53 +0300

    fix(tabs): fix script of tabs changing at index page
    + start styling tabs of platform section
    + fix styling of title in business section

--- src/js/accordion-home.js
+++ src/js/accordion-home.js
@@ -25,17 +25,16 @@ $(() => {
   function handleTabBtnHover(event) {
     const currentTab = $(this);
     const currentTabData = currentTab.attr('data-target');
+    const tabsParent = $(this).closest('.tabs__parent');
     const currContent = $('[data-content="' + currentTabData + '"]');
 
-    tabBtn.removeClass('tab__button--current');
+    tabsParent.find(tabBtn).removeClass('tab__button--current');
     currentTab.addClass('tab__button--current');
 
-    accordionContent
-      .addClass('accordion__content--hidden')
+    tabsParent
+      .find(accordionContent)
       .removeClass('accordion__content--visible');
-    currContent
-      .removeClass('accordion__content--hidden')
-      .addClass('accordion__content--visible');
+    currContent.addClass('accordion__content--visible');
   }
 
   accordionBtn.on('click', handleAccorderonBtnClick);
