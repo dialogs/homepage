1def7f7877f1d265cc9ee8df5ebdc7aa79cec4af
Oleg Shilov <oleg@dlg.im>
2019-03-19 20:38:13 +0300

    fix(download): move links from templates

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@
 
 const path = require('path');
 const express = require('express');
+const links = require('./links');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,8 +16,12 @@ app.get('/features', (req, res) => res.render('../src/features'));
 app.get('/blog', (req, res) => res.render('../src/blog'));
 app.get('/blog/tag', (req, res) => res.render('../src/blogTag'));
 app.get('/about', (req, res) => res.render('../src/about'));
-app.get('/download', (req, res) => res.render('../src/download'));
-app.get('/download/cloud', (req, res) => res.render('../src/downloadCloud'));
+app.get('/download', (req, res) => {
+  res.render('../src/download', { links });
+});
+app.get('/download/cloud', (req, res) => {
+  res.render('../src/downloadCloud', { links });
+});
 app.get('/licenses', (req, res) => res.render('../src/licenses'));
 app.get('/support', (req, res) => res.render('../src/support'));
 app.get('/legal', (req, res) => res.render('../src/legal'));
