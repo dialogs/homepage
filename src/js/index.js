95319ade9ba33355986cbec096cf9a8edd25ede1
Ivan Krasnikov <6051852@gmail.com>
2019-03-20 12:39:42 +0300

    fix: fixed import of css var and importing js files

--- src/js/index.js
+++ src/js/index.js
@@ -7,20 +7,4 @@ import $ from 'jquery';
 import './forms';
 import './features';
 import './accordion-home';
-
-$(function() {
-	console.log('window onload', $(window).width());
-
-	$(window).on('resize', function(e) {
-		console.log($(this), $(this).width());
-	});
-});
-
-$(function() {
-	const headerBtnMemu = $('.header__button--menu');
-	const header = $('#header');
-
-	headerBtnMemu.on('click', function() {
-		header.toggleClass('header--mobile-open');
-	});
-});
+import './interface';
