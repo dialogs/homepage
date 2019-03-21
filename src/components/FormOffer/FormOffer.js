8e58333e4c52fed37694de77b24827bf187e5f41
Ivan Krasnikov <6051852@gmail.com>
2019-03-21 18:56:13 +0300

    feat(offer): create offer component

---
+++ src/components/FormOffer/FormOffer.js
@@ -0,0 +1,147 @@
+import React from 'react';
+
+import { PageHeader } from '../PageHeader/PageHeader';
+import { Section } from '../Section/Section';
+
+import './FormOffer.css';
+
+export function FormOffer() {
+	return (
+		<Section className="offer">
+			<PageHeader>Получить предложение</PageHeader>
+			<div className="offer-description">Пакет от 100 лицензий.</div>
+			<form action="" className="form offer__form">
+				<div className="form-items__box">
+					<div className="form-item form-item--default">
+						<input
+							type="text"
+							className="input--default"
+							id="offer-name"
+							name="OFFER_NAME"
+						/>
+						<label htmlFor="offer-name" className="label--default">
+							Как к вам обращаться
+						</label>
+					</div>
+
+					<div className="form-item form-item--default">
+						<input
+							type="email"
+							className="input--default"
+							id="offer-mail"
+							name="OFFER_EMAIL"
+						/>
+						<label htmlFor="offer-mail" className="label--default">
+							Рабочий e-mail
+						</label>
+					</div>
+
+					<div className="form-item form-item--default">
+						<input
+							type="tel"
+							className="input--default"
+							id="offer-phone"
+							name="OFFER_PHONE"
+						/>
+						<label htmlFor="offer-phone" className="label--default">
+							Телефон
+						</label>
+					</div>
+
+					<div className="form-item form-item--default">
+						<input
+							type="text"
+							className="input--default"
+							id="offer-company"
+							name="OFFER_COMPANY"
+						/>
+						<label htmlFor="offer-company" className="label--default">
+							Название компании
+						</label>
+					</div>
+
+					<div className="form-item form-item--select">
+						<select
+							className="select select--default"
+							id="offer-users"
+							name="OFFER_USERS"
+						>
+							<option disabled>Количество пользователей</option>
+							<option value="1-10">1-10</option>
+							<option value="val2">11-50</option>
+							<option value="val3">51-100</option>
+							<option value="val4">101-500</option>
+							<option value="val5">500+</option>
+						</select>
+					</div>
+				</div>
+
+				<div className="form-items__box">
+					<div className="form-item form-item--checkbox">
+						<label htmlFor="offer-personal" className="checkbox__label">
+							<input
+								defaultChecked
+								className="checkbox__input"
+								type="checkbox"
+								id="offer-personal"
+								name="OFFER_PERSONAL"
+							/>
+							<span className="checkbox__checkbox">
+								<svg
+									width="14"
+									height="12"
+									viewBox="0 0 14 12"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M1 5.65856L4.94027 9.95214L13.1557 1"
+										stroke="#1E1E1E"
+										strokeWidth="2"
+									/>
+								</svg>
+							</span>
+							<span className="checkbox__text">
+								Я согласен на обработку личных данных
+							</span>
+						</label>
+					</div>
+					<div className="form-item form-item--checkbox">
+						<label htmlFor="offer-subscribe" className="checkbox__label">
+							<input
+								className="checkbox__input"
+								type="checkbox"
+								id="offer-subscribe"
+								name="OFFER_SUBSCRIBE"
+							/>
+							<span className="checkbox__checkbox">
+								<svg
+									width="14"
+									height="12"
+									viewBox="0 0 14 12"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										d="M1 5.65856L4.94027 9.95214L13.1557 1"
+										stroke="#1E1E1E"
+										strokeWidth="2"
+									/>
+								</svg>
+							</span>
+							<span className="checkbox__text">
+								Подписаться на новостную рассылку
+							</span>
+						</label>
+					</div>
+				</div>
+
+				<div className="form-item offer__form-item__button">
+					<button type="submit" className="button button--default">
+						Отправить заявку
+					</button>
+				</div>
+			</form>
+		</Section>
+	);
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from 'gatsby';
 import { Container } from '../components/Container/Container';
 import { Platform } from '../components/Platform/Platform';
 import { Incompany } from '../components/Incompany/Incompany';
+import { FormOffer } from '../components/FormOffer/FormOffer';
 import { Section } from '../components/Section/Section';
 import { PageHeader } from '../components/PageHeader/PageHeader';
 // import { Tabs, Tab } from '../components/Tabs/Tabs';
@@ -210,142 +211,7 @@ export default () => {
         <div className="clients__title">Наши клиенты</div>
       </div>
 
-      <div className="home__section offer">
-        <h3 className="offer-title">Получить предложение</h3>
-        <div className="offer-description">Пакет от 100 лицензий.</div>
-        <form action="" className="form offer__form">
-          <div className="offer__form-items-box">
-            <div className="form-item form-item--default offer__form-item">
-              <input
-                type="text"
-                className="input--default"
-                id="offer-name"
-                name="OFFER_NAME"
-              />
-              <label htmlFor="offer-name" className="label--default">
-                Как к вам обращаться
-              </label>
-            </div>
-
-            <div className="form-item form-item--default offer__form-item">
-              <input
-                type="email"
-                className="input--default"
-                id="offer-mail"
-                name="OFFER_EMAIL"
-              />
-              <label htmlFor="offer-mail" className="label--default">
-                Рабочий e-mail
-              </label>
-            </div>
-
-            <div className="form-item form-item--default offer__form-item">
-              <input
-                type="tel"
-                className="input--default"
-                id="offer-phone"
-                name="OFFER_PHONE"
-              />
-              <label htmlFor="offer-phone" className="label--default">
-                Телефон
-              </label>
-            </div>
-
-            <div className="form-item form-item--default offer__form-item">
-              <input
-                type="text"
-                className="input--default"
-                id="offer-company"
-                name="OFFER_COMPANY"
-              />
-              <label htmlFor="offer-company" className="label--default">
-                Название компании
-              </label>
-            </div>
-
-            <div className="form-item form-item--select offer__form-item">
-              <select
-                className="select select--default"
-                id="offer-users"
-                name="OFFER_USERS"
-              >
-                <option disabled>Количество пользователей</option>
-                <option value="1-10">1-10</option>
-                <option value="val2">11-50</option>
-                <option value="val3">51-100</option>
-                <option value="val4">101-500</option>
-                <option value="val5">500+</option>
-              </select>
-            </div>
-          </div>
-
-          <div className="offer__form-items-box offer__form-items-box--checkboxes">
-            <div className="form-item form-item--checkbox offer__form-item">
-              <label htmlFor="offer-personal" className="checkbox__label">
-                <input
-                  defaultChecked
-                  className="checkbox__input"
-                  type="checkbox"
-                  id="offer-personal"
-                  name="OFFER_PERSONAL"
-                />
-                <span className="checkbox__checkbox">
-                  <svg
-                    width="14"
-                    height="12"
-                    viewBox="0 0 14 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M1 5.65856L4.94027 9.95214L13.1557 1"
-                      stroke="#1E1E1E"
-                      strokeWidth="2"
-                    />
-                  </svg>
-                </span>
-                <span className="checkbox__text">
-                  Я согласен на обработку личных данных
-                </span>
-              </label>
-            </div>
-            <div className="form-item form-item--checkbox offer__form-item">
-              <label htmlFor="offer-subscribe" className="checkbox__label">
-                <input
-                  className="checkbox__input"
-                  type="checkbox"
-                  id="offer-subscribe"
-                  name="OFFER_SUBSCRIBE"
-                />
-                <span className="checkbox__checkbox">
-                  <svg
-                    width="14"
-                    height="12"
-                    viewBox="0 0 14 12"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M1 5.65856L4.94027 9.95214L13.1557 1"
-                      stroke="#1E1E1E"
-                      strokeWidth="2"
-                    />
-                  </svg>
-                </span>
-                <span className="checkbox__text">
-                  Подписаться на новостную рассылку
-                </span>
-              </label>
-            </div>
-          </div>
-
-          <div className="form-item offer__form-item offer__form-item__button">
-            <button type="submit" className="button button--default">
-              Отправить заявку
-            </button>
-          </div>
-        </form>
-      </div>
+      <FormOffer />
     </Container>
   );
 };
