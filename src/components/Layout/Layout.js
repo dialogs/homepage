4e84bff9d5835993d65e2b445019aae6b47db283
Ivan Krasnikov <6051852@gmail.com>
2019-03-22 13:59:22 +0300

    wip(modal window): show modal on clicking button

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,24 +3,12 @@ import React from 'react';
 import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
 
-import { FormOffer } from '../FormOffer/FormOffer';
 import '../../styles/index.css';
-import '../../styles/modal.css';
 
 export default ({ children }) => (
 	<main className="main">
 		<Header />
 		{children}
 		<Footer />
-		<section className="modal modal--opened">
-			<div className="modal__bg" />
-			<div className="modal__wrapper">
-				<div className="modal__window">
-					<div className="modal__window__content">
-						<FormOffer />
-					</div>
-				</div>
-			</div>
-		</section>
 	</main>
 );
