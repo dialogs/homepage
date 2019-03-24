3f93e58610b22cfce0ac140f4d5b044a025df758
Oleg Shilov <oleg@dlg.im>
2019-03-24 16:12:12 +0300

    fix: put home page sections back

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import React from 'react';
 import { Page } from '../components/Page/Page';
 import { Container } from '../components/Container/Container';
 import { Hero } from '../components/Hero/Hero';
-// import { Platform } from '../components/Platform/Platform';
-// import { Safety } from '../components/Safety/Safety';
-// import { Incompany } from '../components/Incompany/Incompany';
-// import { Easy } from '../components/Easy/Easy';
-// import { Business } from '../components/Business/Business';
+import { Platform } from '../components/Platform/Platform';
+import { Safety } from '../components/Safety/Safety';
+import { Incompany } from '../components/Incompany/Incompany';
+import { Easy } from '../components/Easy/Easy';
+import { Business } from '../components/Business/Business';
 import { Offer } from '../components/Offer/Offer';
 
 export default () => {
@@ -15,14 +15,13 @@ export default () => {
     <Page>
       <Container>
         <Hero />
+        <Platform />
+        <Safety />
+        <Incompany />
+        <Easy />
+        <Business />
         <Offer />
       </Container>
     </Page>
   );
 };
-
-// <Platform />
-// <Safety />
-// <Incompany />
-// <Easy />
-// <Business />
