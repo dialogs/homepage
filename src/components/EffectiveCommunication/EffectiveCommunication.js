051a3399643a2a7cf1be8f05ad37dfee78e3c56c
Ivan Krasnikov <6051852@gmail.com>
2019-03-26 18:59:36 +0300

    wip(page solution): secureCommunication finished for mobile

---
+++ src/components/EffectiveCommunication/EffectiveCommunication.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Section } from '../Section/Section';
+
+export function EffectiveCommunication() {
+  return (
+    <Section className="effective-communication">
+      <div className="">col1 > title + image</div>
+      <div className="">2 lists</div>
+    </Section>
+  );
+}
