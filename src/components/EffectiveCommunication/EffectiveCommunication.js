c5065c3584b46069401052884a1775857eb00121
Ivan Krasnikov <6051852@gmail.com>
2019-03-26 20:06:24 +0300

    wip(page solution): EffectiveCommunication finished for mobile

--- src/components/EffectiveCommunication/EffectiveCommunication.js
+++ src/components/EffectiveCommunication/EffectiveCommunication.js
@@ -1,12 +1,55 @@
 import React from 'react';
 
 import { Section } from '../Section/Section';
+import { Heading } from '../Heading/Heading';
+
+import './EffectiveCommunication.css';
 
 export function EffectiveCommunication() {
   return (
     <Section className="effective-communication">
-      <div className="">col1 > title + image</div>
-      <div className="">2 lists</div>
+      <div className="column">
+        <Heading level="2">Эффективные коммуникации</Heading>
+        <div className="image">
+          <img
+            src="/images/solution/effective-communication.png"
+            alt="Эффективные коммуникации"
+          />
+        </div>
+      </div>
+      <div className="column column--info">
+        <div className="item">
+          <div className="item__title">
+            Повышение продуктивности сотрудников за счет:
+          </div>
+          <ul className="list list--secondary">
+            <li>
+              Повышения скорости коммуникации в рабочих группах за счет
+              мгновенного обмена сообщениями.
+            </li>
+            <li>
+              Быстрого принятия оперативных решений в рабочих чатах и каналах.
+            </li>
+            <li>
+              Новых возможностей для совместной работы с функциями мессенджера.
+            </li>
+          </ul>
+        </div>
+        <div className="item">
+          <div className="item__title">
+            Сокращение затрат на средства коммуникации от 10% до 30% за счет:
+          </div>
+          <ul className="list list--secondary">
+            <li>Совмещения разрозненных инструментов в одной платформе.</li>
+            <li>Интеграции VoIP и мобильной телефонии.</li>
+            <li>Телеконференций.</li>
+            <li>
+              Экономии при звонках внутри организации, роуминге, звонках на
+              внешние телефоны.
+            </li>
+          </ul>
+        </div>
+      </div>
     </Section>
   );
 }
