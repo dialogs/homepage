cff2c3150c6eec5322d10fc8fff77902d1a52a5d
Ivan Krasnikov <6051852@gmail.com>
2019-04-03 16:26:26 +0300

    fix(licenses): remove section technological partners but create component

--- src/pages/licenses.js
+++ src/pages/licenses.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Container } from '../components/Container/Container';
 import { PageHeader } from '../components/PageHeader/PageHeader';
+// import { PartnersTechnological } from '../components/PartnersTechnological/PartnersTechnological';
 
 export default () => {
   return (
@@ -232,7 +233,7 @@ export default () => {
           </div>
         </section>
 
-        <section className="license__section license__assoc">
+        <section className="license__section license__assoc box--prefooter">
           <div className="license__assoc-title">Реестры и ассоциации</div>
           <ul>
             <li>
@@ -249,87 +250,6 @@ export default () => {
               Информации (ОРИ) Роскомнадзор.
             </li>
           </ul>
-        </section>
-
-        <section className="license__section license__partners box--prefooter">
-          <div className="license__partners-title">
-            Компания имеет технологичных партнеров
-          </div>
-          <div className="license__partners-list">
-            <div className="license__partners-item">
-              <a href="/404" className="license__partners-item-link">
-                <img
-                  className="license__partners-image"
-                  src="/images/partner-infotecs.svg"
-                  alt="infotecs"
-                />
-              </a>
-            </div>
-            <div className="license__partners-item">
-              <a href="/404" className="license__partners-item-link">
-                <img
-                  className="license__partners-image"
-                  src="/images/partner-rostelecom.svg"
-                  alt="rostelecom"
-                />
-              </a>
-            </div>
-            <div className="license__partners-item">
-              <a href="/404" className="license__partners-item-link">
-                <img
-                  className="license__partners-image"
-                  src="/images/partner-norbit.svg"
-                  alt="norbit"
-                />
-              </a>
-            </div>
-            <div className="license__partners-item">
-              <a href="/404" className="license__partners-item-link">
-                <img
-                  className="license__partners-image"
-                  src="/images/partner-jet.svg"
-                  alt="jet"
-                />
-              </a>
-            </div>
-            <div className="license__partners-item">
-              <a href="/404" className="license__partners-item-link">
-                <img
-                  className="license__partners-image"
-                  src="/images/partner-redsoft.svg"
-                  alt="redsoft"
-                />
-              </a>
-            </div>
-            <div className="license__partners-item">
-              <a href="/404" className="license__partners-item-link">
-                <img
-                  className="license__partners-image"
-                  src="/images/partner-omp.svg"
-                  alt="open mobile platform"
-                />
-              </a>
-            </div>
-            <div className="license__partners-item">
-              <a href="/404" className="license__partners-item-link">
-                <img
-                  className="license__partners-image"
-                  src="/images/partner-infowatch.svg"
-                  alt="infowatch"
-                />
-              </a>
-            </div>
-          </div>
-          <div className="license__partners-link-box">
-            <a href="/404" className="license__partners-link link--arrow">
-              <span className="link--arrow__text">Партнерская программа</span>
-              <img
-                src="/images/svg-icons/arrow-link.svg"
-                alt=""
-                className="link--arrow__icon"
-              />
-            </a>
-          </div>
         </section>
       </div>
     </Container>
