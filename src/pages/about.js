2ef0fef4375413ba6f857f0478ec7e7eadf74906
Ivan Krasnikov <6051852@gmail.com>
2019-04-03 17:55:18 +0300

    fix(about): add Novosibirsk second office and add links

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,15 @@ import { Container } from '../components/Container/Container';
 import { PageHeader } from '../components/PageHeader/PageHeader';
 
 export default () => {
+  const officesLinks = {
+    moscow:
+      'https://www.google.com/maps/place/dialog/@55.7368062,37.5323798,18z/data=!3m1!4b1!4m13!1m7!3m6!1s0x0:0x0!2zNTXCsDQ0JzEyLjUiTiAzN8KwMzInMDAuNyJF!3b1!8m2!3d55.736806!4d37.533528!3m4!1s0x46b54a53aa3b42cd:0x3c26303a75a2c356!8m2!3d55.7368062!4d37.5334741',
+    novosibirskNikolaeva:
+      'https://www.google.com/maps/place/%D0%A6%D0%B5%D0%BD%D1%82%D1%80+%D0%B8%D0%BD%D1%84%D0%BE%D1%80%D0%BC%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D1%85+%D1%82%D0%B5%D1%85%D0%BD%D0%BE%D0%BB%D0%BE%D0%B3%D0%B8%D0%B9/@54.8580416,83.1106614,18z/data=!4m13!1m7!3m6!1s0x42dfc3f43cc9190d:0x3da699e7445b010e!2z0YPQuy4g0J3QuNC60L7Qu9Cw0LXQstCwLCAxMSwg0J3QvtCy0L7RgdC40LHQuNGA0YHQuiwg0J3QvtCy0L7RgdC40LHQuNGA0YHQutCw0Y8g0L7QsdC7LiwgNjMwMDkw!3b1!8m2!3d54.8581713!4d83.1103964!3m4!1s0x0:0x70c9334b88f99469!8m2!3d54.8576537!4d83.1117064',
+    novosibirskMost:
+      'https://www.google.com/maps/place/%D0%B1%D0%B8%D0%B7%D0%BD%D0%B5%D1%81-%D1%86%D0%B5%D0%BD%D1%82%D1%80+MOST/@55.023146,82.920614,15z/data=!4m5!3m4!1s0x0:0x6bef1adfc015b0e6!8m2!3d55.023146!4d82.920614',
+  };
+
   return (
     <div className="about page">
       <Container>
@@ -125,7 +134,12 @@ export default () => {
                   121170, ул. Поклонная 3к2, 11 этаж
                 </div>
                 <div className="about__contacts-map-link">
-                  <a href="/404" className="link--default">
+                  <a
+                    href={officesLinks.moscow}
+                    className="link--default"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     На карте
                   </a>
                 </div>
@@ -136,7 +150,28 @@ export default () => {
                   630090, ул. Николаева 11, оф. 311
                 </div>
                 <div className="about__contacts-map-link">
-                  <a href="/404" className="link--default">
+                  <a
+                    href={officesLinks.novosibirskNikolaeva}
+                    className="link--default"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    На карте
+                  </a>
+                </div>
+              </div>
+              <div className="about__contacts-office">
+                <div className="about__contacts-city">Новосибирск</div>
+                <div className="about__contacts-address">
+                  630007, ул. Коммунистическая 40
+                </div>
+                <div className="about__contacts-map-link">
+                  <a
+                    href={officesLinks.novosibirskMost}
+                    className="link--default"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
                     На карте
                   </a>
                 </div>
