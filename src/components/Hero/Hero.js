2ca21ac5d6f4d5d7200e79659a89177d4bde212b
Ivan Krasnikov <6051852@gmail.com>
2019-04-03 15:32:08 +0300

    fix(hero): remove unnecessary markup and styles

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,31 +7,29 @@ import './Hero.css';
 
 export function Hero({ openOfferModal }) {
   return (
-    <Section className="hero home__intro">
-      <div className="home__intro-box">
-        <div className="home__intro-info">
+    <Section className="hero">
+      <div className="hero__wrapper">
+        <div className="hero__content">
           <PageHeader>
             Достигайте командные цели быстрее и без потери фокуса
           </PageHeader>
-          <h4 className="home__intro-text">
+          <h4 className="hero__text">
             Простая и эффективная платформа для коммуникаций в компании
           </h4>
           <button
             type="button"
-            className="button button--default home__intro-button"
+            className="button button--default"
             onClick={openOfferModal}
           >
             Получить предложение
           </button>
         </div>
-        <div className="home__intro-image">
-          <img
-            className="home__intro-img"
-            src="/images/home/home-hero.png"
-            srcSet="/images/home/home-hero@2x.png 2x"
-            alt="Достигайте командные цели быстрее и без потери фокуса"
-          />
-        </div>
+        <img
+          className="hero__image"
+          src="/images/home/home-hero.png"
+          srcSet="/images/home/home-hero@2x.png 2x"
+          alt="Достигайте командные цели быстрее и без потери фокуса"
+        />
       </div>
     </Section>
   );
