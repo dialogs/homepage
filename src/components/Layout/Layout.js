92c7c02249ea7e9cfefaa2644fb10b85f09a69db
Ivan Krasnikov <6051852@gmail.com>
2019-04-09 12:30:35 +0300

    feat(meta): add default title+description meta to layout

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { IntlProvider, addLocaleData } from 'react-intl';
+import Helmet from 'react-helmet';
 
 import Header from '../Header';
 import Modals from '../Modals';
@@ -13,9 +14,25 @@ addLocaleData(localeData);
 export default ({ children, pageContext }) => {
   const { locale = 'en', languages, originalPath } = pageContext;
 
+  const metaData = {
+    title:
+      locale === 'ru'
+        ? 'dialog | Платформа для простых и эффективных коммуникаций'
+        : 'dialog | A platform for simple and efficient communications',
+    description:
+      locale === 'ru'
+        ? 'это корпоративный мессенджер с возможностью установки на внутренний сервер организации'
+        : 'handy and feature-rich enterprise multi-device messenger available for server or cloud – Slack-like, but not Slack-limited',
+  };
+
   return (
     <IntlProvider locale={locale} messages={messages[locale]}>
       <main className="main">
+        <Helmet>
+          <title>{metaData.title}</title>
+          <meta name="description" content={metaData.description} />
+        </Helmet>
+
         <Header locale={locale} originalPath={originalPath} />
         {children}
         <Footer locale={locale} />
