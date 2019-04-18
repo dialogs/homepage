bd49dfc29d5f0b53fa460bea37934f4f152e7375
Ivan Krasnikov <6051852@gmail.com>
2019-04-18 11:07:33 +0300

    feat(og tags): create component for open graph tags localization

---
+++ src/components/FormattedOpenGraph/FormattedOpenGraph.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
+export function FormattedOpenGraph({
+  intl: { messages },
+  ogType = 'website',
+  idOgTitle = 'og_title_default',
+  idOgDescription = 'og_description_default',
+  ogImageSrc = '/images/dialog.jpg',
+}) {
+  const ogTitle = messages[idOgTitle];
+  const ogDescription = messages[idOgDescription];
+  const ogUrl = window.location.href;
+  const ogImage = `${window.location.origin}${ogImageSrc}`;
+
+  return (
+    <Helmet>
+      <meta property="og:type" content={ogType} />
+      <meta property="og:title" content={ogTitle} />
+      <meta property="og:description" content={ogDescription} />
+      <meta property="og:url" content={ogUrl} />
+      <meta property="og:image" content={ogImage} />
+      <meta property="og:site_name" content="dialog" />
+    </Helmet>
+  );
+}
