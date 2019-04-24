5e0652a50426fa98ad9ee8563ad4246e5e01eb40
Ivan Krasnikov <6051852@gmail.com>
2019-04-24 15:03:48 +0300

    fix(open graph): check for window object

--- src/components/FormattedOpenGraph/FormattedOpenGraph.js
+++ src/components/FormattedOpenGraph/FormattedOpenGraph.js
@@ -10,21 +10,21 @@ export function FormattedOpenGraph({
 }) {
   const ogTitle = messages[idOgTitle];
   const ogDescription = messages[idOgDescription];
-  let ogUrl;
-  let ogImage;
-
-  if (typeof window !== `undefined`) {
-    ogUrl = window.location.href;
-    ogImage = `${window.location.origin}${ogImageSrc}`;
-  }
 
   return (
     <Helmet>
       <meta property="og:type" content={ogType} />
       <meta property="og:title" content={ogTitle} />
       <meta property="og:description" content={ogDescription} />
-      <meta property="og:url" content={ogUrl} />
-      <meta property="og:image" content={ogImage} />
+      {typeof window !== 'undefined'
+        ? [
+            <meta property="og:url" content={window.location.href} />,
+            <meta
+              property="og:image"
+              content={`${window.location.origin}${ogImageSrc}`}
+            />,
+          ]
+        : null}
       <meta property="og:site_name" content="dialog" />
     </Helmet>
   );
