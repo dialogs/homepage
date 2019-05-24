2f365013995d3111bd813ebdd9baa36878bf4d97
Oleg Shilov <oleg@dlg.im>
2019-05-24 14:48:25 +0300

    chore: rename license image

--- src/pages/patents.js
+++ src/pages/patents.js
@@ -185,7 +185,7 @@ export default ({ pageContext: { locale, url, originalPath } }) => {
               <div className="license__photos-link-box">
                 <a
                   className="link--download license__photos-link"
-                  href="/images/license-fsb.jpg"
+                  href="/images/license-fstek.jpg"
                   download
                 >
                   <span className="link--download__text">
