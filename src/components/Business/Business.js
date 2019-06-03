c02355cfa0727f17a196a8f740d70a6d0ff6882c
Oleg Shilov <oleg@dlg.im>
2019-06-03 17:23:31 +0300

    feat: more images to homepage

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { StaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';
 import ImageFormatted from '../ImageFormatted';
 
 import { Section } from '../Section/Section';
@@ -9,50 +11,73 @@ import './Business.css';
 
 export function Business({ intl: { locale } }) {
   return (
-    <Section className="business">
-      <div className="business__cols">
-        <div className="business__info">
-          <Heading className="business__info-title">
-            <FormattedMessage id="business_header" />
-          </Heading>
-          <div className="business__info-subtitle">
-            <FormattedMessage id="business_example" />
-          </div>
-          <div className="business__info-logo">
-            <img
-              className="business__info-img"
-              src={
-                locale === 'ru'
-                  ? '/images/home/business/logo-sberbank-business.svg'
-                  : '/images/home/business/logo-sberbank-en.svg'
+    <StaticQuery
+      query={graphql`
+        query {
+          image: file(
+            relativePath: { eq: "images/home/business/man2@2x.png" }
+          ) {
+            childImageSharp {
+              fluid(maxWidth: 230) {
+                ...GatsbyImageSharpFluid_withWebp_noBase64
               }
-              alt={locale === 'ru' ? 'Сбербанк' : 'Sberbank'}
-            />
-          </div>
-          <ul className="business__info-list list list--secondary">
-            <li className="business__info-item">
-              <FormattedMessage id="business_case_1" />
-            </li>
-            <li className="business__info-item">
-              <FormattedMessage id="business_case_2" />
-            </li>
-            <li className="business__info-item">
-              <FormattedMessage id="business_case_3" />
-            </li>
-            <li className="business__info-item">
-              <FormattedMessage id="business_case_4" />
-            </li>
-          </ul>
-        </div>
-        <div className="business__image">
-          <ImageFormatted
-            imgClass="business__img"
-            src="/images/home/business/man2@2x.png"
-            srcSet="/images/home/business/man2@2x.png 2x"
-            altLangId="alt_big_business"
-          />
-        </div>
-      </div>
-    </Section>
+            }
+          }
+        }
+      `}
+      render={({ image }) => {
+        return (
+          <Section className="business">
+            <div className="business__cols">
+              <div className="business__info">
+                <Heading className="business__info-title">
+                  <FormattedMessage id="business_header" />
+                </Heading>
+                <div className="business__info-subtitle">
+                  <FormattedMessage id="business_example" />
+                </div>
+                <div className="business__info-logo">
+                  <img
+                    className="business__info-img"
+                    src={
+                      locale === 'ru'
+                        ? '/images/home/business/logo-sberbank-business.svg'
+                        : '/images/home/business/logo-sberbank-en.svg'
+                    }
+                    alt={locale === 'ru' ? 'Сбербанк' : 'Sberbank'}
+                  />
+                </div>
+                <ul className="business__info-list list list--secondary">
+                  <li className="business__info-item">
+                    <FormattedMessage id="business_case_1" />
+                  </li>
+                  <li className="business__info-item">
+                    <FormattedMessage id="business_case_2" />
+                  </li>
+                  <li className="business__info-item">
+                    <FormattedMessage id="business_case_3" />
+                  </li>
+                  <li className="business__info-item">
+                    <FormattedMessage id="business_case_4" />
+                  </li>
+                </ul>
+              </div>
+              <div className="business__image">
+                <FormattedMessage id="alt_big_business">
+                  {(alt) => (
+                    <Image
+                      fadeIn
+                      className="business__img"
+                      fluid={image.childImageSharp.fluid}
+                      alt={alt}
+                    />
+                  )}
+                </FormattedMessage>
+              </div>
+            </div>
+          </Section>
+        );
+      }}
+    />
   );
 }
--- src/components/Easy/Easy.js
+++ src/components/Easy/Easy.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { StaticQuery, graphql } from 'gatsby';
+import Image from 'gatsby-image';
 import ImageFormatted from '../ImageFormatted';
 
 import { Section } from '../Section/Section';
@@ -9,42 +11,65 @@ import './Easy.css';
 
 export function Easy() {
   return (
-    <Section className="easy">
-      <div className="easy__cols">
-        <div className="easy__col easy__col--left">
-          <Heading className="easy-title">
-            <FormattedMessage id="easy_header" />
-          </Heading>
-          <div className="easy-image">
-            <ImageFormatted
-              imgClass="easy-img"
-              src="/images/home/easy/easy-export@2x.png"
-              srcSet="/images/home/easy/easy-export@2x.png 2x"
-              altLangId="alt_easy"
-            />
-          </div>
-        </div>
-        <div className="easy__col easy__col--right">
-          <div className="easy-info">
-            <div className="easy-info-item">
-              <div className="easy-info-title">
-                <FormattedMessage id="easier_title" />
+    <StaticQuery
+      query={graphql`
+        query {
+          image: file(
+            relativePath: { eq: "images/home/easy/easy-export@2x.png" }
+          ) {
+            childImageSharp {
+              fluid(maxWidth: 600) {
+                ...GatsbyImageSharpFluid_withWebp_noBase64
+              }
+            }
+          }
+        }
+      `}
+      render={({ image }) => {
+        return (
+          <Section className="easy">
+            <div className="easy__cols">
+              <div className="easy__col easy__col--left">
+                <Heading className="easy-title">
+                  <FormattedMessage id="easy_header" />
+                </Heading>
+                <div className="easy-image">
+                  <FormattedMessage id="alt_easy">
+                    {(alt) => (
+                      <Image
+                        fadeIn
+                        className="easy-img"
+                        fluid={image.childImageSharp.fluid}
+                        alt={alt}
+                      />
+                    )}
+                  </FormattedMessage>
+                </div>
               </div>
-              <div className="easy-info-text">
-                <FormattedMessage id="easier_text" />
+              <div className="easy__col easy__col--right">
+                <div className="easy-info">
+                  <div className="easy-info-item">
+                    <div className="easy-info-title">
+                      <FormattedMessage id="easier_title" />
+                    </div>
+                    <div className="easy-info-text">
+                      <FormattedMessage id="easier_text" />
+                    </div>
+                  </div>
+                  <div className="easy-info-item">
+                    <div className="easy-info-title">
+                      <FormattedMessage id="more_efficient_title" />
+                    </div>
+                    <div className="easy-info-text">
+                      <FormattedMessage id="more_efficient_text" />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="easy-info-item">
-              <div className="easy-info-title">
-                <FormattedMessage id="more_efficient_title" />
-              </div>
-              <div className="easy-info-text">
-                <FormattedMessage id="more_efficient_text" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Section>
+          </Section>
+        );
+      }}
+    />
   );
 }
