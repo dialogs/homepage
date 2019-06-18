00f100a8bbfa9c33a9dccebbc99a533ef80018be
Denis <krnvdns@outlook.com>
2019-06-18 14:08:30 +0400

    Heading image change

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,13 +1,22 @@
 import React from 'react';
 
 import { Section } from '../Section/Section';
+import { PageHeader } from '../PageHeader/PageHeader';
+import { FormattedMessage } from 'react-intl';
 
 import './Promo.css';
 
-export function Promo({ children }) {
+export function Promo() {
   return (
     <Section className="promo">
-      <div className="promo__content">{children}</div>
+      <div className="promo__content">
+        <PageHeader className="solution">
+          <FormattedMessage id="solutions_simple" />
+        </PageHeader>
+        <div className="promo__image">
+          <img className="promo__img" src="/images/solution/promo_img.svg" />
+        </div>
+      </div>
     </Section>
   );
 }
