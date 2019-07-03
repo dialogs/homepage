7010b3e69ee776d4eba6bd830dcc47512e20582d
Oleg Shilov <oleg@dlg.im>
2019-07-03 22:12:25 +0300

    fix: more typo fixes

--- src/components/Modal/ModalHeader.js
+++ src/components/Modal/ModalHeader.js
@@ -5,7 +5,7 @@ import { Heading } from '../Heading/Heading';
 export function ModalHeader({ children }) {
   return (
     <div className="modal__window__header">
-      <Heading>{children}</Heading>
+      <Heading level="1">{children}</Heading>
     </div>
   );
 }
