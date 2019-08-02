c4b73249b1d4d60d6ce62e4340dc23648f526c44
Oleg Shilov <oleg@dlg.im>
2019-08-02 19:02:15 +0300

    refactor(blog): update blog post card styles
    code cleanup

--- src/components/BlogPostCard/BlogPostCard.js
+++ src/components/BlogPostCard/BlogPostCard.js
@@ -1,9 +1,83 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import removeMarkdown from 'remove-markdown';
-
+import { styled } from 'astroturf';
 import { Tag } from '../Tag/Tag';
+import { Text } from '../Text/Text';
 import { removeServiceTags } from '../../utils/removeServiceTags';
+
+const BlogPostCardContainer = styled.div`
+  @import '../../styles/variables.css';
+
+  margin-top: 30px;
+  position: relative;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 60px;
+    left: -20px;
+    right: -20px;
+    bottom: -20px;
+    background: var(--color-primary);
+    z-index: -1;
+    opacity: 0;
+    transition: all 300ms ease-out;
+    transform: translateZ(0);
+    box-shadow: 0px 4px 24px color-mod(#000 alpha(5%));
+  }
+
+  &:hover {
+    &:before {
+      opacity: 1;
+      top: 80px;
+      bottom: 0;
+    }
+  }
+`;
+
+const BlogPostCardImage = styled.img`
+  width: 100%;
+`;
+
+const BlogPostCardDate = styled.div`
+  @import '../../styles/variables.css';
+
+  margin-top: 20px;
+  font-size: 14px;
+  line-height: 18px;
+  color: var(--text-color-main-03);
+
+  @media (--tablet-landscape-viewport) {
+    margin-top: 30px;
+  }
+`;
+
+const BlogPostCardTitle = styled(Text).attrs({ bold: true, size: 'large' })``;
+
+const BlogPostCardExcerpt = styled(Text).attrs({ bold: true })`
+  padding-top: 0;
+`;
+
+const BlogPostCardLink = styled(Link)`
+  display: inline-block;
+  height: 100%;
+`;
+
+const BlogPostCardTags = styled.div`
+  @import '../../styles/variables.css';
+
+  margin-top: 20px;
+  margin-left: -15px;
+
+  & span {
+    display: inline-block;
+    margin-left: 15px;
+    font-size: 14px;
+    line-height: 18px;
+    color: var(--text-color-main-03);
+  }
+`;
 
 export function BlogPostCard({
   title,
@@ -13,32 +87,31 @@ export function BlogPostCard({
   excerpt,
   tags,
   locale,
+  className,
 }) {
   const tagsToRender = removeServiceTags(tags);
 
   return (
-    <div className="blog__roster-item">
-      <Link to={`/${locale}/blog/${slug}`} className="blog__roster-item-link">
-        <div className="blog__roster-image">
-          <img src={featureImage} alt={title} />
-        </div>
-        <div className="blog__roster-date">
+    <BlogPostCardContainer className={className}>
+      <BlogPostCardLink to={`/${locale}/blog/${slug}`}>
+        <BlogPostCardImage src={featureImage} alt={title} />
+        <BlogPostCardDate>
           {new Date(publishDate).toLocaleDateString(locale, {
             year: 'numeric',
             month: 'long',
             day: 'numeric',
           })}
-        </div>
-        <div className="blog__roster-name">{title}</div>
-        <div className="blog__roster-short">{removeMarkdown(excerpt)}</div>
+        </BlogPostCardDate>
+        <BlogPostCardTitle>{title}</BlogPostCardTitle>
+        <BlogPostCardExcerpt>{removeMarkdown(excerpt)}</BlogPostCardExcerpt>
         {tagsToRender && (
-          <div className="blog__roster-tags">
+          <BlogPostCardTags>
             {tagsToRender.map(({ name, id }) => (
               <Tag key={id} name={name} />
             ))}
-          </div>
+          </BlogPostCardTags>
         )}
-      </Link>
-    </div>
+      </BlogPostCardLink>
+    </BlogPostCardContainer>
   );
 }
--- src/components/BlogRoster/BlogRoster.js
+++ src/components/BlogRoster/BlogRoster.js
@@ -1,10 +1,38 @@
 import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
-
+import { styled } from 'astroturf';
 import { Section } from '../Section/Section';
 import { BlogPostCard } from '../BlogPostCard/BlogPostCard';
+import { Button } from '../Button/Button';
+import { Heading } from '../Heading/Heading';
 
 const DEFAULT_LIMIT = 6;
+
+const BlogRosterList = styled.div`
+  @import '../../styles/variables.css';
+
+  margin-bottom: 2rem;
+
+  @media (--tablet-viewport) {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+  }
+`;
+
+const BlogRosterCard = styled(BlogPostCard)`
+  @import '../../styles/variables.css';
+
+  @media (--tablet-viewport) {
+    margin-top: 40px;
+    flex: 0 0 47%;
+  }
+
+  @media (--tablet-landscape-viewport) {
+    margin-top: 60px;
+    flex: 0 0 28%;
+  }
+`;
 
 export function BlogRoster({ title, posts, limit = DEFAULT_LIMIT, locale }) {
   const [visibleCount, updateVisibleCount] = useState(limit);
@@ -14,34 +42,27 @@ export function BlogRoster({ title, posts, limit = DEFAULT_LIMIT, locale }) {
   }
 
   return (
-    <Section className="blog__roster-section">
-      <div className="blog__roster">
-        <div className="blog__roster-title">{title}</div>
-        <div className="blog__roster-list">
-          {posts.slice(0, visibleCount).map((post) => {
-            return (
-              <BlogPostCard
-                key={post.id}
-                slug={post.slug}
-                title={post.title}
-                excerpt={post.excerpt}
-                tags={post.tags}
-                publishDate={post.publishDate}
-                featureImage={post.featureImage}
-                locale={locale}
-              />
-            );
-          })}
-        </div>
-        {visibleCount < posts.length && (
-          <button
-            className="button button--pale blog__button-more"
-            onClick={handleLoadMoreClick}
-          >
-            <FormattedMessage id="blog_roster_see_more" />
-          </button>
-        )}
-      </div>
+    <Section>
+      <Heading>{title}</Heading>
+      <BlogRosterList>
+        {posts.slice(0, visibleCount).map((post) => (
+          <BlogRosterCard
+            key={post.id}
+            slug={post.slug}
+            title={post.title}
+            excerpt={post.excerpt}
+            tags={post.tags}
+            publishDate={post.publishDate}
+            featureImage={post.featureImage}
+            locale={locale}
+          />
+        ))}
+      </BlogRosterList>
+      {visibleCount < posts.length && (
+        <Button theme="pale" wide onClick={handleLoadMoreClick}>
+          <FormattedMessage id="blog_roster_see_more" />
+        </Button>
+      )}
     </Section>
   );
 }
