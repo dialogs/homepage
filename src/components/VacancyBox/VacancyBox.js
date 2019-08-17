1f47eaf5c4390166331ff8780c544ec15ce6996f
Oleg Shilov <oleg@dlg.im>
2019-08-18 02:59:35 +0300

    fix(VacancyBox): fix skill tags

--- src/components/VacancyBox/VacancyBox.js
+++ src/components/VacancyBox/VacancyBox.js
@@ -2,20 +2,16 @@ import React from 'react';
 import { Link } from 'gatsby';
 import './VacancyBox.css';
 
-export function VacancyBox({ title, link, tags }) {
+export function VacancyBox({ title, link, tags, skills }) {
+  console.log(skills, skills.length);
   return (
     <Link to={link} className="vacancies__box">
       <div className="vacancies__box__inner">
         <div className="vacancies__box__title">{title}</div>
         <div className="vacancies__box__tags">
-          {tags &&
-            tags
-              .filter((tag) => tag.length)
-              .map((tag) => (
-                <span key={tag}>
-                  {tag} <span className="tag-delimeter">&sdot;</span>{' '}
-                </span>
-              ))}
+          {skills.map((skill) => (
+            <span key={skill.name}>{skill.name}</span>
+          ))}
         </div>
       </div>
     </Link>
