72e99916d4cf7a6aafb2d2ccf83e045b08929b37
Oleg Shilov <oleg@dlg.im>
2019-08-18 03:11:59 +0300

    chore+fix(VacancyBox): remove unnecessary log and add `skills.length` check

--- src/components/VacancyBox/VacancyBox.js
+++ src/components/VacancyBox/VacancyBox.js
@@ -3,15 +3,14 @@ import { Link } from 'gatsby';
 import './VacancyBox.css';
 
 export function VacancyBox({ title, link, tags, skills }) {
-  console.log(skills, skills.length);
   return (
     <Link to={link} className="vacancies__box">
       <div className="vacancies__box__inner">
         <div className="vacancies__box__title">{title}</div>
         <div className="vacancies__box__tags">
-          {skills.map((skill) => (
-            <span key={skill.name}>{skill.name}</span>
-          ))}
+          {skills.length
+            ? skills.map((skill) => <span key={skill.name}>{skill.name}</span>)
+            : null}
         </div>
       </div>
     </Link>
