f94df362d6f4728b650fc5cccf4e8a036cb6fba7
Artiom Ponomariov <artyponomarty@gmail.com>
2019-09-10 18:29:41 +0300

    fix(Organize): moved styles for list items from another component

--- src/components/Organize/Organize.js
+++ src/components/Organize/Organize.js
@@ -3,76 +3,92 @@ import './Organize.css';
 import { Section } from '../Section/Section';
 import { FormattedMessage } from 'react-intl';
 import { ArrowLink } from '../ArrowLink/ArrowLink';
+import { Heading } from '../Heading/Heading';
 
 export function Organize() {
   return (
-    <Section className="organize">
+    <Section className="organize organize--min-height">
       <div className="organize__wrapper">
         <div className="organize__row">
           <div className="organize__col organize__col--supervisor">
-            <h2 className="organize__title">
+            <Heading level="2" className="prices__description">
               <FormattedMessage id="organize_heading_1" />
-            </h2>
-            <h3 className="organize__subtitle">
-              <FormattedMessage id="organize_title_1_1" />
-            </h3>
-            <ul className="organize__list list">
-              <li className="organize__item">
-                <FormattedMessage id="organize_text_1_1_1" />
-              </li>
-              <li className="organize__item">
-                <FormattedMessage id="organize_text_1_1_2" />
-              </li>
-            </ul>
-            <h3 className="organize__subtitle">
-              <FormattedMessage id="organize_title_1_2" />
-            </h3>
-            <ul className="organize__list list">
-              <li className="organize__item">
-                <FormattedMessage id="organize_text_1_2_1" />
-              </li>
-            </ul>
+            </Heading>
 
-            <h3 className="organize__subtitle">
-              <FormattedMessage id="organize_title_1_3" />
-            </h3>
-            <ul className="organize__list list">
-              <li className="organize__item">
-                <FormattedMessage id="organize_text_1_3_1" />
-              </li>
-            </ul>
+            <div className="organize__group item ">
+              <Heading level="3" className="item__title">
+                <FormattedMessage id="organize_title_1_1" />
+              </Heading>
+              <ul className="organize__list list">
+                <li className="organize__item">
+                  <FormattedMessage id="organize_text_1_1_1" />
+                </li>
+                <li className="organize__item">
+                  <FormattedMessage id="organize_text_1_1_2" />
+                </li>
+              </ul>
+            </div>
+
+            <div className="organize__group item ">
+              <Heading level="3" className="item__title">
+                <FormattedMessage id="organize_title_1_2" />
+              </Heading>
+              <ul className="organize__list list">
+                <li className="organize__item">
+                  <FormattedMessage id="organize_text_1_2_1" />
+                </li>
+              </ul>
+            </div>
+
+            <div className="organize__group item ">
+              <Heading level="3" className="item__title">
+                <FormattedMessage id="organize_title_1_3" />
+              </Heading>
+              <ul className="organize__list list">
+                <li className="organize__item">
+                  <FormattedMessage id="organize_text_1_3_1" />
+                </li>
+              </ul>
+            </div>
           </div>
 
           <div className="organize__col organize__col--employee">
-            <h2 className="organize__title">
+            <Heading level="2" className="prices__description">
               <FormattedMessage id="organize_heading_2" />
-            </h2>
-            <h3 className="organize__subtitle">
-              <FormattedMessage id="organize_title_2_1" />
-            </h3>
-            <ul className="organize__list list list">
-              <li className="organize__item">
-                <FormattedMessage id="organize_text_2_1_1" />
-              </li>
-            </ul>
+            </Heading>
 
-            <h3 className="organize__subtitle">
-              <FormattedMessage id="organize_title_2_2" />
-            </h3>
-            <ul className="organize__list list">
-              <li className="organize__item">
-                <FormattedMessage id="organize_text_2_2_1" />
-              </li>
-            </ul>
+            <div className="organize__group item ">
+              <Heading level="3" className="item__title">
+                <FormattedMessage id="organize_title_2_1" />
+              </Heading>
+              <ul className="organize__list list">
+                <li className="organize__item">
+                  <FormattedMessage id="organize_text_2_1_1" />
+                </li>
+              </ul>
+            </div>
 
-            <h3 className="organize__subtitle">
-              <FormattedMessage id="organize_title_2_3" />
-            </h3>
-            <ul className="organize__list list">
-              <li className="organize__item">
-                <FormattedMessage id="organize_text_2_3_1" />
-              </li>
-            </ul>
+            <div className="organize__group item ">
+              <Heading level="3" className="item__title">
+                <FormattedMessage id="organize_title_2_2" />
+              </Heading>
+              <ul className="organize__list list">
+                <li className="organize__item">
+                  <FormattedMessage id="organize_text_2_2_1" />
+                </li>
+              </ul>
+            </div>
+
+            <div className="organize__group item ">
+              <Heading level="3" className="item__title">
+                <FormattedMessage id="organize_title_2_3" />
+              </Heading>
+              <ul className="organize__list list">
+                <li className="organize__item">
+                  <FormattedMessage id="organize_text_2_3_1" />
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
