c0ebc53b0ae0423ba64107d3b1b71f4b17db117a
Artiom Ponomariov <artyponomarty@gmail.com>
2019-09-22 03:55:38 +0300

    fix(News): responsivness

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -5,23 +5,26 @@ import { NewsCard } from './NewsCard';
 import { PageHeading } from '../PageHeading/PageHeading';
 import { FormattedMessage } from 'react-intl';
 import { ArrowLink } from '../ArrowLink/ArrowLink';
+import { Container } from '../Container/Container';
 
 export function News({ language }) {
   return (
-    <Section className="news stretch stretch--news">
-      <PageHeading className="news__title">
-        <FormattedMessage id="news_title" />
-      </PageHeading>
-      <div className="blog__roster-list">
-        <NewsCard index="1" date="1.10.12" language="ru" />
-        <NewsCard index="2" date="1.10.12" language="ru" />
-        <NewsCard index="3" date="1.10.12" language="ru" />
-      </div>
-      <div className="news__link">
-        <ArrowLink to={`/${language}/about`} primary-new>
-          <FormattedMessage id="news_link" />
-        </ArrowLink>
-      </div>
+    <Section className="news stretch">
+      <Container>
+        <PageHeading className="news__title">
+          <FormattedMessage id="news_title" />
+        </PageHeading>
+        <div className="blog__roster-list">
+          <NewsCard index="1" date="1.10.12" language="ru" />
+          <NewsCard index="2" date="1.10.12" language="ru" />
+          <NewsCard index="3" date="1.10.12" language="ru" />
+        </div>
+        <div className="news__link">
+          <ArrowLink to={`/${language}/about`} underline primary-new>
+            <FormattedMessage id="news_link" />
+          </ArrowLink>
+        </div>
+      </Container>
     </Section>
   );
 }
